ddd6a6bcb3519f3d1a4dcad36d72a32522546b58
Ankush <akrk053@gmail.com>
2026-08-30 19:43:34 +0530

    Time: 0 ms (100%), Space: 9 MB (37.37%) - LeetHub-Neo

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,42 @@
+
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            // Opening bracket
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+
+            // Closing bracket
+            else {
+
+                if (st.empty()) {
+                    return false;
+                }
+
+                char top = st.top();
+                st.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
